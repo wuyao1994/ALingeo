b3a525186fdeb272d9ed212c4c894f9139ebf23a
wuyao1994 <wuyaoiot@163.com>
2018-06-10 17:24:59 +0800

    add news page

--- alingeoWeb/src/main/java/com/alingeo/controller/AdminController.java
+++ alingeoWeb/src/main/java/com/alingeo/controller/AdminController.java
@@ -1,18 +1,13 @@
 package com.alingeo.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import com.alingeo.domin.News;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.alingeo.domin.News;
 import com.alingeo.service.JoinService;
 import com.alingeo.service.NewsService;
 import com.alingeo.util.FileUtil;
@@ -52,12 +47,12 @@ public class AdminController {
 
 
 
-	@RequestMapping(value = "/imageUpload", method = RequestMethod.POST,produces = "application/json;")
+	@RequestMapping(value = "/imageUpload", method = RequestMethod.POST)
 	public String upLoadAccessory(@RequestParam("file") MultipartFile file, @RequestParam("id") Long id) {
 		String fileName = file.getOriginalFilename();
 		try {
 			FileUtil.uploadFile(file.getBytes(), location, fileName);
-			String iamgePath = location+fileName;
+			String iamgePath = location + fileName;
 			News news = newsService.findNewsById(id);
 			news.setImagePath(iamgePath);
 			newsService.udpateNews(news);
@@ -65,6 +60,29 @@ public class AdminController {
 			e.printStackTrace();
 		}
 
+		return "redirect:/admin/news";
+	}
+
+
+
+	@RequestMapping(value = "/admin/deleteNews/{id}", method = RequestMethod.GET)
+	public String deleteNews(@PathVariable("id") Long id, Model model) {
+		newsService.deleteNewsById(id);
+		return "redirect:/admin/news";
+	}
+
+
+
+	@RequestMapping(value = "/admin/addNews", method = RequestMethod.GET)
+	public String addNewsPage(Model model) {
+		return "addNews";
+	}
+
+
+
+	@RequestMapping(value = "/admin/addNews", method = RequestMethod.POST)
+	public String addNews(Model model, @ModelAttribute News news) {
+		newsService.createNews(news);
 		return "redirect:/admin/news";
 	}
 }
