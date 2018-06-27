e5f0a5ebe5fbe20a27b9c1fe08278281d4729426
elviswu <elviswu@aaxischina.com>
2018-06-27 11:46:55 +0800

    jpa select

--- alingeoWeb/src/main/resources/static/js/all.js
+++ alingeoWeb/src/main/resources/static/js/all.js
@@ -3,27 +3,42 @@ $(function() {
        var from = $(".join-form"),
             url = "/JoinUs/create",
             data = from.serialize();
-       $.post(url, data, function (result) {
-           if (result == "success") {
-               alert("提交成功");
-               window.location.reload();
-           }
+       if (!check()) {
+           return false;
+       } else {
+           $.post(url, data, function (result) {
+               if (result == "success") {
+                   alert("提交成功");
+                   window.location.reload();
+               }
 
-       })
+           })
+       }
     });
-    $("#telephone").keyup(function () {
 
-        var phone_v = $(this).val();
+    function check(){
+        var name = $.trim($("#name").val());
+        var mobile = $.trim($("#telephone").val());
+        var email = $.trim($("#email").val());
+        var comment = $.trim($("#content").val());
+        var myreg1 = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+        var myreg2 = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
 
-        if (phone_v.length >= 11) {
-
-            $(this).val(phone_v.substr(0, 11));
-
+        if(name == "") {
+            alert("姓名不能为空！");
+            return false;
         }
-
-        $(this).val($(this).val().replace(/[^0-9.]/g, ''));
-
-    });
-
-
+        if(!myreg1.test(mobile)) {
+            alert("请输入正确的手机号！");
+            return false;
+        }
+        if(!myreg2.test(email)) {
+            alert("请输入有效的邮箱！");
+            return false;
+        }
+        if(comment == "") {
+            alert("内容不能为空！");
+            return false;
+        }
+    }
 });
