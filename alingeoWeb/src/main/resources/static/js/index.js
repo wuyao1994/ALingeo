1d09e55398c2c5a457bf14875c04a4816892f873
wuyao1994 <wuyaoiot@163.com>
2018-06-29 00:15:02 +0800

    all

--- alingeoWeb/src/main/resources/static/js/index.js
+++ alingeoWeb/src/main/resources/static/js/index.js
@@ -2,23 +2,26 @@
    $(".lesson1").mouseenter(function(){
    	  $(".lesson1 img").addClass("animated bounceIn");
    	  setTimeout('$(".lesson1 img").removeClass("animated bounceIn")','500')
-   })
+   });
    $(".lesson2").mouseenter(function(){
    	  $(".lesson2 img").addClass("animated bounceIn");
    	  setTimeout('$(".lesson2 img").removeClass("animated bounceIn")','500')
-   })
+   });
    $(".lesson3").mouseenter(function(){
    	  $(".lesson3 img").addClass("animated bounceIn");
    	  setTimeout('$(".lesson3 img").removeClass("animated bounceIn")','500')
-   })
+   });
    $(".lesson-btn").mouseenter(function(){
    	   $(this).addClass("animated swing");
    	  setTimeout('$(".lesson-btn").removeClass("animated swing")','500')
-   })
+   });
 	$(".a-icon").mouseenter(function(){
    	   $(this).addClass("animated rotateIn");
    	  setTimeout('$(".a-icon").removeClass("animated rotateIn")','500')
-   })
-	
+   });
+	 $(".about-btn").mouseenter(function(){
+		 $(this).addClass("animated swing");
+		 setTimeout('$(".about-btn").removeClass("animated swing")','500')
+	 });
  });
  
--- alingeoWeb/src/main/resources/static/js/news.js
+++ alingeoWeb/src/main/resources/static/js/news.js
@@ -1,6 +1,41 @@
 $(function(){
-	 $(".show li a img").mouseenter(function(){
-   	  $(this).addClass("animated rotateIn");
-   	  setTimeout('$(".show li a img").removeClass("animated rotateIn")','500')
-   })
+	$(".show li a img").mouseenter(function(){
+		$(this).addClass("animated rotateIn");
+		setTimeout('$(".show li a img").removeClass("animated rotateIn")','500')
+	});
+    $(function(){
+        //总数目
+        var length = $("#hiddenresult .show").length;
+
+        $("#Pagination").pagination(length, {
+            num_edge_entries: Math.ceil(length/4),
+            num_display_entries: 2,
+            callback: pageselectCallback,
+            items_per_page:4
+        });
+
+        // 按钮事件
+        $("#setoptions").click(function(){
+            $("#Pagination").pagination(length, {
+                num_edge_entries: Math.ceil(length/4),
+                num_display_entries: 2,
+                callback: pageselectCallback,
+                items_per_page:4
+            });
+        });
+
+        function pageselectCallback(page_index, jq){
+            // 从表单获取每页的显示的列表项数目
+            var items_per_page = 4;
+            var max_elem = Math.min((page_index+1) * items_per_page, length);
+
+            $("#Searchresult ul").html("");
+            // 获取加载元素
+            for(var i=page_index*items_per_page;i<max_elem;i++){
+                $("#Searchresult ul").append($("#hiddenresult .show:eq("+i+")").clone());
+            }
+            //阻止单击事件
+            return false;
+        }
+    });
 });
