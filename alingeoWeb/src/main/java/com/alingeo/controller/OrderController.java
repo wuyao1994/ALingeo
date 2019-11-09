af491d50d9089704d0ae096863078d3c3b7680aa
wuyao1994 <wuyaoiot@163.com>
2019-11-10 01:33:34 +0800

    pansd

--- alingeoWeb/src/main/java/com/alingeo/controller/OrderController.java
+++ alingeoWeb/src/main/java/com/alingeo/controller/OrderController.java
@@ -1,11 +1,13 @@
 package com.alingeo.controller;
 
-import com.alingeo.domin.Order;
-import com.alingeo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alingeo.domin.Order;
+import com.alingeo.service.OrderService;
 
 @RestController
 @RequestMapping("/order")
@@ -13,7 +15,7 @@ public class OrderController {
     @Autowired
     private OrderService mOrderService;
     @PostMapping("/getOrder")
-    public Object getOrder(String id) {
+    public Object getOrder(@RequestParam("id") String id) {
         Order order = mOrderService.findByOrderNumber(id);
         return order;
     }
